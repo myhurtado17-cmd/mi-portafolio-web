2da7b0c227d68162c28795f84dbb264c6ed6cc34
Marco <myhurtado17@gmail.com>
2025-11-25 15:30:32 -0500

    despliegue en vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/server';
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [react(), tailwind()],
+    output: 'server', // para SSR
+    adapter: vercel(),
 });
